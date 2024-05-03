4f65d720d1d1f1279f6ce9e65924a329f86c613f
entaku <entaku19890818@gmail.com>
2024-05-03 19:14:08 +0900

    analiticsを導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:footballnews/NewsListScreen.dart';
 import 'package:footballnews/VideoPlayerScreen.dart';
-import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-
-void main() {
+void main() async{
+WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
